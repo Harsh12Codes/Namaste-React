52c15f9c6c60df1469882a263b444a7e64c545ac
Harsh12Codes <harsh12codes@gmail.com>
2024-01-02 08:30:00 +0530

    update App.js, Inception/index-javascript.html and Inception/index-react.css

--- Inception/App.js
+++ Inception/App.js
@@ -1,3 +1,13 @@
+//React.createElement return a object
 const heading = React.createElement("h1", { id: "heading" }, "Hello world from React");
+const description = React.createElement(
+    "p",
+    { id: "description" },
+    "This is pure react we are not using HTMX,we are using javascript to create all these tags",
+);
+const child = React.createElement("div", { id: "child" }, [heading, description]);
+const parent = React.createElement("div", { id: "parent" }, child);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+//This render function take the object and convert it into a HTML tag
+root.render(parent);
